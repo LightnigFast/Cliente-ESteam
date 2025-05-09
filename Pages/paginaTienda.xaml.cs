3e83c469faa01ca8f515967497f64b3313a33c8b
LightningFast <bydraxx4k129@gmail.com>
2025-05-09 19:45:16 +0200

    v0.0.0.3
    Carrusel mas o menos preparado para la inserción de datos

--- Pages/paginaTienda.xaml.cs
+++ Pages/paginaTienda.xaml.cs
@@ -20,9 +20,47 @@ namespace Cliente_TFG.Pages
     /// </summary>
     public partial class paginaTienda : Page
     {
+
+        private string[] imagenesCarrusel;
+        private int indiceActual = 0;
+
+
         public paginaTienda()
         {
             InitializeComponent();
+
+            imagenesCarrusel = new string[]
+            {
+                "https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_616x353.jpg",
+                "https://cdn.akamai.steamstatic.com/steam/apps/570/capsule_616x353.jpg",
+                "https://cdn.akamai.steamstatic.com/steam/apps/440/capsule_616x353.jpg"
+            };
+
+            MostrarImagenActual();
         }
+
+        private void MostrarImagenActual()
+        {
+            imagenTiendaGrande.Source = new BitmapImage(new Uri(imagenesCarrusel[indiceActual]));
+        }
+
+        private void BtnAnterior_Click(object sender, RoutedEventArgs e)
+        {
+            if (indiceActual > 0)
+            {
+                indiceActual--;
+                MostrarImagenActual();
+            }
+        }
+
+        private void BtnSiguiente_Click(object sender, RoutedEventArgs e)
+        {
+            if (indiceActual < imagenesCarrusel.Length - 1)
+            {
+                indiceActual++;
+                MostrarImagenActual();
+            }
+        }
+
     }
 }
