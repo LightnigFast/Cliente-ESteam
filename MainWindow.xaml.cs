44b84bec23eac53c203177e6be5d3068bc5d50dc
rubenhs9 <rhsilveira9@gmail.com>
2025-05-25 13:29:18 +0200

    vuelta al usuario 3 jeje

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -48,7 +48,7 @@ namespace Cliente_TFG
 
             if (online)
             {
-                CargarDatosUsuario(14);
+                CargarDatosUsuario(3);
                 Cabecera_top.NombreUsuario = user.NombreUsuario;
                 Cabecera_top.Dinero = user.Dinero;
             }
