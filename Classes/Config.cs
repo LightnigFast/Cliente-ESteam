34d43559a568d606ac4aeb04f1228a72f03fab8b
LightningFast <bydraxx4k129@gmail.com>
2025-05-28 18:40:12 +0200

    v0.0.4.5.1
    Nueva lista de ips

--- Classes/Config.cs
+++ Classes/Config.cs
@@ -9,6 +9,13 @@ namespace Cliente_TFG.Classes
     public static class Config
     {
         public static string IP { get; set; } = "127.0.0.1";
-        //public static string IP { get; set; } = "26.84.183.227";
+
+        //public static string IP { get; set; } = "26.84.183.227"; //JESUS PC
+        //public static string IP { get; set; } = "26.254.119.213"; //JESUS LAPTOP
+
+        //public static string IP { get; set; } = "26.75.134.118"; //RUBEN PC
+        //public static string IP { get; set; } = "26.75.134.118"; //RUBEN LAPTOP
+
+        //public static string IP { get; set; } = "26.72.223.97"; //NACHO
     }
 }
