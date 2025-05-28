0d18fbc47a7d9782bb3a580eb017c2039e5d8c45
LightningFast <bydraxx4k129@gmail.com>
2025-05-28 13:34:43 +0200

    v.0.0.4.2
    Cambios en el login

--- UserControls/LoginControl.xaml.cs
+++ UserControls/LoginControl.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,7 +24,8 @@ namespace Cliente_TFG.UserControls
     {
         public delegate void RegistroRequested();
         public event RegistroRequested AbrirRegistro;
-
+        //public string ip = "26.84.183.227";
+        public string ip = "127.0.0.1";
 
 
         public LoginControl()
@@ -34,6 +37,64 @@ namespace Cliente_TFG.UserControls
         {
             AbrirRegistro?.Invoke();
         }
+
+        private async void BotonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string correo = txtCorreo.Text;
+            string contrasena = txtPassword.Password;
+
+            bool exito = await LoginUsuarioAsync(correo, contrasena);
+            if (exito)
+            {
+                //NAVEGAR A LA APP O MOSTRAR LA VENTANA PRINCIPAL
+            }
+        }
+
+        public async Task<bool> LoginUsuarioAsync(string correo, string contrasena)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var datos = new
+                {
+                    correo = correo,
+                    contrasena = contrasena
+                };
+
+                var contenido = new StringContent(
+                    JsonConvert.SerializeObject(datos),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync("http://" + ip + ":50000/login/", contenido);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string respuestaJson = await response.Content.ReadAsStringAsync();
+                        dynamic datosRespuesta = JsonConvert.DeserializeObject(respuestaJson);
+                        string idUsuario = datosRespuesta.id_usuario;
+
+                        MessageBox.Show("Login correcto. ID: " + idUsuario);
+                        return true;
+                    }
+                    else
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show("Error en login: " + error);
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR DE CONEXIÓN: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+
 
     }
 }
